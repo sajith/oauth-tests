92df978e21574e25a8a21898d87219f084efa488
Sajith Sasidharan <sajith@nonzen.in>
2015-09-30 16:51:12 -0400

    Update comments in polls/urls.py

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     # /polls/
     url(r'^$', views.IndexView.as_view(), name='index'),
 
-    # /polls/5/
+    # /polls/[[:number:]]/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # # /polls/5/results
+    # /polls/[[:number:]]/results
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 
-    # # /polls/5/vote
+    # /polls/[[:number:]]/vote
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
