d2446efa7751c67da7f27006b63314402580197a
Sajith Sasidharan <sajith@nonzen.in>
2015-10-01 16:08:33 -0400

    Fix formatting consistency.

--- django/polls/tests.py
+++ django/polls/tests.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 from .models import Question
 
 class QuestionMethodTests(TestCase):
+
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() should return False for questions whose
