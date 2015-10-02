77033d15bbcd28b6c8867346ebac4f648e145345
Sajith Sasidharan <sajith@nonzen.in>
2015-10-02 18:21:44 -0400

    Add UserViewSet and GroupViewSet.

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
+
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -51,3 +55,21 @@ def vote(request, question_id):
     # POST data will prevent data from being posted twice, if/when
     # user hits the back button.
     return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+# ------------------------------------------------------------
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+# ------------------------------------------------------------    
