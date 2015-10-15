3dca8ae0367a1d0433bbb2d01267a104a3f643df
Sajith Sasidharan <sajith@nonzen.in>
2015-10-15 15:57:04 -0400

    Update tutorial/urls.py

--- django/tutorial/urls.py
+++ django/tutorial/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework import routers
+from tutorial.quickstart import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tutorial.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
