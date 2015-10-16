95ad08b82eb3b692a4dd65dc79668a08ae645240
Sajith Sasidharan <sajith@nonzen.in>
2015-10-16 11:48:43 -0400

    Fix urlpatterns.

--- django/tutorial/urls.py
+++ django/tutorial/urls.py
@@ -7,6 +7,6 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
