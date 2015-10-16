7e4ba93cdc8b72cd3e1bf08c2724946a2f2097fd
Sajith Sasidharan <sajith@nonzen.in>
2015-10-16 15:11:48 -0400

    Add snippets.urls route

--- django/tutorial/urls.py
+++ django/tutorial/urls.py
@@ -8,5 +8,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include('snippets.urls')),
+    # url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
