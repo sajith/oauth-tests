f584c35c467e29ebdcae222cfe71a23b3d3dc9d1
Sajith Sasidharan <sajith@nonzen.in>
2015-10-20 15:52:50 -0400

    Add snippets urlpatterns.

--- django/snippets/urls.py
+++ django/snippets/urls.py
@@ -1,4 +1,7 @@
-# from django.conf.urls import url, include
+from django.conf.urls import url
+from snippets import views
 
 urlpatterns = [
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
